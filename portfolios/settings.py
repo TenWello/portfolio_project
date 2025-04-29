3373cb06541411a6af7a312d4562c9700ccf7ae3
TenWello <jahongirfayziyev94@gmail.com>
2025-04-29 21:27:43 +0500

    New changes

--- portfolios/settings.py
+++ portfolios/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'portfolios.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'USER': os.getenv('USER'),
