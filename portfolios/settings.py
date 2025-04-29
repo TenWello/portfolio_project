15e7d8bbd54d9399dd51f0524c7d1263300e6c25
TenWello <jahongirfayziyev94@gmail.com>
2025-04-29 21:12:24 +0500

    New changes

--- portfolios/settings.py
+++ portfolios/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolios.settings')
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
