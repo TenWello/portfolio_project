fbf35c64fd14cd0bbbc20828b651efa2d608a777
TenWello <jahongirfayziyev94@gmail.com>
2025-06-16 17:03:18 +0500

    First Deploy

--- portfolios/wsgi.py
+++ portfolios/wsgi.py
@@ -1,7 +1,11 @@
-
 import os
+import sys
+
+path = '/home/tenwello/portfolio_project'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'portfolios.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolios.settings')
-
 application = get_wsgi_application()
